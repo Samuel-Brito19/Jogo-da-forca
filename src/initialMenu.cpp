a12e04e6fbd46ae3c19402f198001a8d5e2978b5
Samuel-Brito19 <sambriton7@gmail.com>
2024-03-12 19:11:52 -0300

    Initial menu

--- src/initialMenu.cpp
+++ src/initialMenu.cpp
@@ -1,12 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+void cleanScreen()
+{
+    system("CLS");
+}
+
+void initialMenu()
+{
+    int option = 0;
+    while ((option < 1 || option > 3))
+    {
+        cleanScreen();
+        cout << "Bem vindo ao jogo";
+        cout << "\n1 - Jogar";
+        cout << "\n2 - Sobre";
+        cout << "\n3 - Sair";
+        cout << "\n Escolha uma opção e tecle enter: ";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            /* code */
+            cout << "Jogo iniciado";
+            break;
+        case 2:
+            /* code */
+            cout << "Informaçoes do jogo";
+            break;
+        case 3:
+            /* code */
+            cout << "Ate mais";
+            break;
+        }
+    }
+}
+
 int main()
 {
-    printf("hello world");
 
+    initialMenu();
     return 0;
 }
